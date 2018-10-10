1974a02651d77985b5f65c57c1575ca0e23753fa
彦回 <yanhui.dh@antfin.com>
2018-10-10 15:37:03 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Pivot, PivotItem, Label } from 'office-ui-fabric-react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-link'>wings</h1>
         </header>
+        <Pivot>
+          <PivotItem
+            headerText='公告'
+            headerButtonProps={{
+              'data-order': 1,
+              'data-title': '公告'
+            }}
+          >
+            <Label>公告</Label>
+          </PivotItem>
+          <PivotItem headerText='课程列表'>
+            <Label>课程列表</Label>
+          </PivotItem>
+          <PivotItem headerText='已选课程'>
+            <Label>已选课程</Label>
+          </PivotItem>
+        </Pivot>
       </div>
     );
+  }
+  click() {
+    console.log("clicked!");
   }
 }
 
